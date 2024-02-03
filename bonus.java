32df0aed3d53303a87e3c17d72961e02b3cbc6c7
HarliHaan <157319625+HarliHaan@users.noreply.github.com>
2024-02-03 20:13:03 +0200

    Add files via upload

---
+++ bonus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class bonus {
+    public static void main(String[] args) {
+
+        double radius, area;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle");
+
+        radius = input.nextDouble();
+
+        area = Math.PI * radius * radius;
+        System.out.println(" Area of the circle is "+ area);
+
+
+
+    }
+}
